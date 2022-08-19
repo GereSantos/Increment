d3c4e3fa3242e5e295b2d183977bc317aa4556ba
Geremias Santos <Geremias99999@gmail.com>
2022-08-19 14:27:57 -0300

    Using Arrow Functions for increment

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Sobre o Mini- Projeto
+
+Arrow Function utilizada para fazer um incremento de dois valores.
+
+## Linguagens Utilizadas
+
+- JavaScript
---
+++ script.js
@@ -0,0 +1,4 @@
+const increment = (number, value  = 1) => number + value;
+//Arrow Function utilizada para incrementar dois valores.
+
+console.log(increment(5,2))
